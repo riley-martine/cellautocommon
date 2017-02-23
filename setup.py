087e910b3f93a18294caa4d464995f97fe08c5de
riley <riley.martine@protonmail.com>
2017-02-23 17:08:53 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="cellautocommon",
+      version="0.1",
+      description="Useful files/functions for cellular automata",
+      url="http://github.com/riley-martine/cellautocommon",
+      author="Riley Martine",
+      author_email="riley.martine.0@gmail.com",
+      license="MIT",
+      packages=["cellautocommon"],
+      zip_safe=False)
