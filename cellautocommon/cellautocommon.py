8173e4cc72398ef8c48caf20b470093c33bba342
riley <maxm215@gmail.com>
2017-02-23 19:46:22 -0500

    Merge RuleList into main

--- cellautocommon/cellautocommon.py
+++ cellautocommon/cellautocommon.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 """Functions for working with cellular automata."""
 from typing import List
-from rulelist import RuleList
+
+class RuleList(object):
+    """class for 1D cell auto rules"""
+
+    def __init__(self, rule):  # rules are in the form of a number from 0-255
+        self.rule = rule
+        b_rule_string = format(rule, '08b')
+        b_rule_list = [char for char in b_rule_string]
+
+        self.rules = {
+            "111": b_rule_list[0],
+            "110": b_rule_list[1],
+            "101": b_rule_list[2],
+            "100": b_rule_list[3],
+            "011": b_rule_list[4],
+            "010": b_rule_list[5],
+            "001": b_rule_list[6],
+            "000": b_rule_list[7]
+        }
+
+    def __repr__(self):
+        return "RuleList({})".format(self.rule)
 
 
 def get_next_row(row: str, rule: int=None, rule_dict: RuleList=None) -> str:
